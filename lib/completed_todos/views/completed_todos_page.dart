8147b1fe79df1fa9a5517e1f022ce51c2bd15f20
Marcos Sevilla <marcossevilla14@gmail.com>
2021-05-24 13:22:18 -0600

    added pages for the views 👀

--- lib/completed_todos/views/completed_todos_page.dart
+++ lib/completed_todos/views/completed_todos_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-class TodosPage extends StatelessWidget {
+class CompletedTodosPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('This is TodosPage'),
+        child: Text('This is CompletedTodosPage'),
       ),
     );
   }
